bf7f8a8a1cb945ae0e84deddfda3a36134326a19
= <=>
2017-08-25 15:49:31 -0300

    Alterando classe de conexão

--- general/helpers/Conexao.php
+++ general/helpers/Conexao.php
@@ -6,15 +6,18 @@ namespace general\helpers;
 class Conexao{
 
 	private $PDO;
+	private $dns = 'mysql:host=localhost;port=3306;dbname=social_work;charset=utf8';
+	private $user = 'root';
+	private $pass = '';
 
 	function __construct()
 	{
-		$this->PDO = new \PDO('mysql:host=localhost;dbname=social_work;charset=utf8', 'root', ''); //Conexão
+		$this->PDO = new \PDO($this->dns,$this->user,$this->pass); //Conexão
 		$this->PDO->setAttribute( \PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION ); //habilitando erros do PDO
 	}
 
 	function retornaConexao(){
-		return $this->PDO;
+		return $this->PDO;	
 	}
 	
 }
